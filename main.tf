c4f6119adc6539de373f93869a81f3f24e9715e4
Dennizz <root@ipaddr.nl>
2022-03-31 16:30:49 +0200

    count issue

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_dx_transit_virtual_interface" "default" {
   count = var.dx_gateway_id == "" ? 1 : 0
 
   connection_id  = var.aws_dx_connection
-  dx_gateway_id  = aws_dx_gateway.default.id
+  dx_gateway_id  = aws_dx_gateway[0].default.id
   name           = var.name != "" ? var.name : "transit_vif_${var.region}"
   vlan           = var.vlan
   address_family = "ipv4"

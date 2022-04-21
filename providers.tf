8419b4c00c580a8ff31e66fdf5c1c9fbc087f6d1
Dennizz <root@ipaddr.nl>
2022-04-21 11:40:33 +0200

    test aws account optionality

--- providers.tf
+++ providers.tf
@@ -2,4 +2,5 @@ provider "aws" {
   #Disabling credential validation to deal with use case where no AWS resources need to be deployed (dx_gateway_id is provided) and AWS credentials are not set up.
   skip_credentials_validation = true
   skip_region_validation      = true
+  skip_requesting_account_id  = true
 }
